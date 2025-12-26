ee262f40024e4f6b61fff6f4ebd95869a8036877
Srinath Nuthalapati <srynath13@gmail.com>
2025-12-26 23:38:35 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // This matches your axios baseURL '/api'
+      "/api": {
+        target: "http://3.29.249.5:3000", // Your Backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
